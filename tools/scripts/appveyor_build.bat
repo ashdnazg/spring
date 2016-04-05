b54deb253a70f92178a11e9dc80ed13e07a3647c
Eshed <ashdnazg@gmail.com>
2016-04-06 02:36:24 +0300

    Update appveyor_build.bat

--- tools/scripts/appveyor_build.bat
+++ tools/scripts/appveyor_build.bat
@@ -2,8 +2,6 @@ SET "root=%CD%"
 SET "build_dir=%root%\build"
 MD %build_dir%
 
-SET BOOST_ROOT=C:\Libraries\boost_1_59_0
-
 :Setup
   PUSHD %root%
     ECHO Cloning vclibs
@@ -14,7 +12,7 @@ SET BOOST_ROOT=C:\Libraries\boost_1_59_0
   
 :Generate
   PUSHD %build_dir%
-    cmake .. -DAI_TYPES=NATIVE
+    cmake .. -DAI_TYPES=NATIVE -DBOOST_INCLUDEDIR=C:/Libraries/boost_1_59_0/include
     msbuild Spring.sln /target:engineSim /p:Configuration=%configuration% /m:2 /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
   POPD
 EXIT /b
